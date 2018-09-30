c70f169e169a4dfa0ddd040d33bf7e21c2369334
Eka Risyana <risyana@gmail.com>
2018-10-01 05:50:14 +0700

    refactor: follow eslint rules

--- script.js
+++ script.js
@@ -1,106 +1,100 @@
-// API 
-const URL = 'https://en.wikipedia.org/w/api.php';
-const OPTION = '?action=query&list=search&utf8=&format=json&srsearch='
+// API
+const ENDPOINT = 'https://en.wikipedia.org/w/api.php';
+const OPTION = '?action=query&list=search&utf8=&format=json&srsearch=';
 
 // Local Storage Model
 
 /*
- *  query: '',
  *  searchResult: {
  *    { query1: { search: [],
  *                searchInfo: {}
  *                }
  *    },
  *  },
- * 
+ *
  */
 
 // Local Storage
 
 const initStorage = () => {
   console.log('init storage');
-  const store = window.localStorage; 
-  if (!store.searchResult){
+  const store = window.localStorage;
+  if (!store.searchResult) {
     store.setItem('searchResult', '{}');
   }
-}
+};
 
 const storage = window.localStorage;
 
-// MODEL
-let query = 'hello'; 
-let searchResult = null; // { query1: {...}, query2: {...}}
-
 // populate DOM
-const myForm = document.querySelector(".myform");
-const inputText = document.querySelector("[name='query']");
-const listContainer = document.querySelector(".listContainer");
-
-// add event listener
-myForm.addEventListener('submit', async function(e) {
-  query = inputText.value;
-  e.preventDefault();
-  if(!query) {
-    return null;
-  }
-  init();
-});
+const myForm = document.querySelector('.myform');
+const listContainer = document.querySelector('.listContainer');
 
 // checking existence in local storage
-const isExistInLocalStorage = function (query) {
-  console.log("check data existence in local storage");
-  //console.log(JSON.parse(storage['searchResult']), query);
-  if (JSON.parse(storage['searchResult']).hasOwnProperty(query)) {
+const isExistInLocalStorage = (searchKey) => {
+  console.log('check data existence in local storage');
+  const currentStorage = JSON.parse(storage.searchResult);
+  const hasSearchKey = Object.prototype.hasOwnProperty.call(currentStorage, searchKey);
+  if (hasSearchKey) {
     return true;
   }
   return false;
-}
+};
 
 // set new data to local storage
-const setDataToLocalStorage = function(query, data) {
-  console.log("write to local storage");
-  const oldSearchResult = JSON.parse(storage['searchResult']);
-  console.log(oldSearchResult)
-  const updatedSearchResult = {...oldSearchResult, [query]: data}
-  console.log(updatedSearchResult)
+const setDataToLocalStorage = (query, data) => {
+  console.log('write to local storage');
+  const oldSearchResult = JSON.parse(storage.searchResult);
+  const updatedSearchResult = { ...oldSearchResult, [query]: data };
   storage.setItem('searchResult', JSON.stringify(updatedSearchResult));
-}
+};
 
 // fetch data
-const fetchData = async function () {
+const fetchData = async (url) => {
   console.log('fetch');
   try {
-    let result = await fetch(`${URL}${OPTION}${query}`);
+    let result = await fetch(url);
     result = await result.json();
     return result;
   } catch (error) {
-    console.log(error);
-    return null;
+    return error;
   }
-}
+};
 
 
 // render
-const render = function(articles) {
+const render = (articles, container) => {
   console.log('render');
-  listContainer.innerHTML = '';
-  articles.forEach(article => {
+  while (container.firstChild) container.removeChild(container.firstChild);
+  articles.forEach((article) => {
     const div = document.createElement('div');
     const text = document.createTextNode(article.title);
     div.appendChild(text);
-    listContainer.appendChild(div);
+    container.appendChild(div);
   });
-}
+};
 
-// init
-const init = async function() {
+// main
+const main = async (query) => {
   await initStorage();
   const isExist = isExistInLocalStorage(query);
-  if(!isExist) {
-    const result = await fetchData();
+  if (!isExist) {
+    const result = await fetchData(`${ENDPOINT}${OPTION}${query}`);
     await setDataToLocalStorage(query, result.query);
   }
-  render(JSON.parse(storage['searchResult'])[query].search);
-}
+  render(JSON.parse(storage.searchResult)[query].search, listContainer);
+};
 
-init();
+// submit handler
+const submitHandler = async (e) => {
+  const searchKey = e.target.elements.query.value;
+  e.preventDefault();
+  if (!searchKey) return null;
+  await main(searchKey);
+  return true;
+};
+
+// add event listener
+myForm.addEventListener('submit', submitHandler);
+
+main('hello world');
