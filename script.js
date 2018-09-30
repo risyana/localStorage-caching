774694f23b12bd957231212007512262a7a34f5e
Eka Risyana <risyana@gmail.com>
2018-09-30 18:52:28 +0700

    save result to localStorage, get data from localStorage when available

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const OPTION = '?action=query&list=search&utf8=&format=json&srsearch='
 // Local Storage Model
 
 /*
- * 
- * wikipediaSearchApp : {
  *  query: '',
  *  searchResult: {
  *    { query1: { search: [],
@@ -14,12 +12,23 @@ const OPTION = '?action=query&list=search&utf8=&format=json&srsearch='
  *                }
  *    },
  *  },
- * }
  * 
  */
 
+// Local Storage
+
+const initStorage = () => {
+  console.log('init storage');
+  const store = window.localStorage; 
+  if (!store.searchResult){
+    store.setItem('searchResult', '{}');
+  }
+}
+
+const storage = window.localStorage;
+
 // MODEL
-let query = ''; 
+let query = 'hello'; 
 let searchResult = null; // { query1: {...}, query2: {...}}
 
 // populate DOM
@@ -34,16 +43,35 @@ myForm.addEventListener('submit', async function(e) {
   if(!query) {
     return null;
   }
-  const result = await fetchData();
-  render(result.query.search);
+  init();
 });
 
+// checking existence in local storage
+const isExistInLocalStorage = function (query) {
+  console.log("check data existence in local storage");
+  //console.log(JSON.parse(storage['searchResult']), query);
+  if (JSON.parse(storage['searchResult']).hasOwnProperty(query)) {
+    return true;
+  }
+  return false;
+}
+
+// set new data to local storage
+const setDataToLocalStorage = function(query, data) {
+  console.log("write to local storage");
+  const oldSearchResult = JSON.parse(storage['searchResult']);
+  console.log(oldSearchResult)
+  const updatedSearchResult = {...oldSearchResult, [query]: data}
+  console.log(updatedSearchResult)
+  storage.setItem('searchResult', JSON.stringify(updatedSearchResult));
+}
+
 // fetch data
-const fetchData = async function() {
+const fetchData = async function () {
+  console.log('fetch');
   try {
     let result = await fetch(`${URL}${OPTION}${query}`);
     result = await result.json();
-    console.log(result);
     return result;
   } catch (error) {
     console.log(error);
@@ -54,6 +82,7 @@ const fetchData = async function() {
 
 // render
 const render = function(articles) {
+  console.log('render');
   listContainer.innerHTML = '';
   articles.forEach(article => {
     const div = document.createElement('div');
@@ -63,4 +92,15 @@ const render = function(articles) {
   });
 }
 
+// init
+const init = async function() {
+  await initStorage();
+  const isExist = isExistInLocalStorage(query);
+  if(!isExist) {
+    const result = await fetchData();
+    await setDataToLocalStorage(query, result.query);
+  }
+  render(JSON.parse(storage['searchResult'])[query].search);
+}
 
+init();
