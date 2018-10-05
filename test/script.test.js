b9ec8ac9c826a6d3a44adf7b5b23ccef343fc090
Eka Risyana <risyana@gmail.com>
2018-10-05 13:28:41 +0700

    add test

--- test/script.test.js
+++ test/script.test.js
@@ -2,6 +2,8 @@ const chai = require("chai");
 const { APP } = require("../source/script/script");
 
 const storage = window.localStorage;
+const MY_FORM = document.querySelector(".myform");
+const LIST_CONTAINER = document.querySelector(".listContainer");
 
 const { expect } = chai;
 
@@ -13,6 +15,7 @@ describe("APP", () => {
     });
     it("should return null");
   });
+
   describe("is Exist in Local Storage ?", () => {
     before(() => {
       const data = { search: [], searchInfo: {} };
@@ -20,23 +23,58 @@ describe("APP", () => {
     });
     it("should return true", () => {
       const result = APP.test.isExistInLocalStorage(storage, "hello");
-      expect(result).to.be.true;
+      return expect(result).to.be.true;
     });
     it("should return false", () => {
       const result = APP.test.isExistInLocalStorage(storage, "xman");
-      expect(result).to.be.false;
+      return expect(result).to.be.false;
     });
     after(() => {
-      storage.clear();
+      storage.setItem("searchResult", "{}");
     });
   });
+
   describe("Fetch data from API", () => {
-    it("should return object");
-    it("should return error");
+    const VALID_ENDPOINT = "https://en.wikipedia.org/w/api.php";
+    const INVALID_ENDPOINT = "https://en.wikipediaxxxxxxxx.org/w/api.php";
+    const OPT =
+      "?action=query&list=search&utf8=&format=json&origin=*&srsearch=";
+    const searchKey = "Jakarta";
+    it("should return object", async () => {
+      const result = await APP.test.fetchData(
+        `${VALID_ENDPOINT}${OPT}${searchKey}`
+      );
+      expect(result).to.be.an("object");
+      expect(result).to.have.property("continue");
+      expect(result).to.have.property("query");
+    });
+    it("should return error", async () => {
+      const result = await APP.test.fetchData(
+        `${INVALID_ENDPOINT}${OPT}${searchKey}`
+      );
+      expect(result).to.be.an("object");
+      expect(result).to.have.property("message");
+      expect(result).to.have.property("type");
+      expect(result).to.have.property("errno");
+      expect(result).to.have.property("code");
+    });
   });
+
   describe("Set data to localStorage", () => {
-    it("Data should be set to localStorage");
+    it("Data should be set to localStorage", () => {
+      const searchKey = "Some Search Key";
+      const data = { search: [{ title: "sample" }], searchInfo: { x: "x" } };
+      const result = APP.test.setDataToLocalStorage(storage, searchKey, data);
+      expect(result).to.be.true;
+      expect(JSON.parse(storage.getItem("searchResult"))).to.have.property(
+        searchKey
+      );
+    });
+    after(() => {
+      storage.setItem("searchResult", "{}");
+    });
   });
+
   describe("Render", () => {
     describe("Check Paremeter", () => {
       it("articles should be an array of object");
